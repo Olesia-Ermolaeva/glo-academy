fd72f4807c132a7b45e9e59f9dd7b345b520d3e9
Виктор Малышев <ovmalisheva@gmail.com>
2020-03-02 11:41:50 +0300

    added minimization CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -10,3 +12,11 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+// Clean CSS
+gulp.task('mincss', function() {
+  return gulp.src("src/css/*.css")
+  .pipe(rename({suffix: ".min"}))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest("app/css"));
+  })
