a1f56759569322505634ea24ae1679a9402e60fb
Виктор Малышев <ovmalisheva@gmail.com>
2020-03-13 20:32:08 +0300

    feat: add button Up

--- js/main.js
+++ js/main.js
@@ -25,3 +25,13 @@ $(document).ready(function () {
     modal.toggleClass('modal--visible');
   });
 });
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+      if ($('#upbutton').is(':hidden')) {
+          $('#upbutton').css({opacity : 1}).fadeIn('slow');
+      }
+  } else { $('#upbutton').stop(true, false).fadeOut('fast'); }
+});
+$('#upbutton').click(function() {
+  $('html, body').stop().animate({scrollTop : 0}, 300);
+});
